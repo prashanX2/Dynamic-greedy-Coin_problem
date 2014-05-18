dcc3e5b92f2ba739448e8d2b22608fb3c47d0f8a
prashanX2 <prashan@compsoc.lk>
2014-05-18 16:26:22 +0530

    commit_1

---
+++ Dyanimc_greedy_coin_problem/Question2/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Question2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(683, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(729, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "25";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 34);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(396, 288);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(414, 34);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(204, 360);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(16, 341);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(392, 53);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dynamic Solution all Possibilities";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(411, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Greedy Solution Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dynamic Solution Optimal Output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(680, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Amount";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(624, 34);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox5.Size = new System.Drawing.Size(142, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "1 2 5 10 25 50 100";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(689, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Denominations";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 406);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Question2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Dyanimc_greedy_coin_problem/Question2/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Question2
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        #region Varibles
+        /*data holders for Dynamic method*/
+        int[,] optimal;
+        string[,] optCoin;
+        List<string> allPossibilities = new List<string>();
+        int dynAmount;
+        int[] denominations;
+
+        /*data holders for greedy method*/
+        Dictionary<int, int> greedyCoins = new Dictionary<int, int>();
+        int[] coins = new int[] { 1, 2, 5, 10, 25, 50, 100 };
+        int coinAmount = 72;
+
+        bool run = false;
+
+
+        #endregion
+
+
+        #region Dynamic Methods
+
+        /*Display all possible Solutions of dynamic method in to textbox*/
+        public void displayAllPossibilities ()
+        {
+            if(allPossibilities.Count > 0)
+            {
+
+                int i = 1;
+                foreach(string s in allPossibilities)
+                {
+                    SetText2(i + ") " + s + "\n");
+                    i++;
+                }
+                SetText2("\n");
+            }
+            else
+            {
+                SetText2("No change for dynAmount=" + dynAmount + ", Denominations=" + denomiStr() + "\n");
+            }
+
+        }
+
+        /*returns the optimal SOlution of the all found solutions by dynamic method*/
+        public String DynamicOptimal ()
+        {
+            int i = optCoin.GetLength(0);
+            int j = optCoin.GetLength(1);
+            String str = optCoin[i - 1, j - 1].Trim();
+
+            return str;
+        }
+
+        /*build teh denominations into a string*/
+        public string denomiStr ()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(int i in denominations)
+            {
+                sb.Append(i + " ");
+            }
+            return sb.ToString();
+        }
+
+        /*method to find all the possible solutions recursively*/
+        private void dynamicAllPossiblitiesRecursive (string tmpstr, int sIndex, int remaining)
+        {
+            for(int i = sIndex; i < denominations.Length; i++)
+            {
+                int temp = remaining - denominations[i];
+                String tempStr = tmpstr + "" + denominations[i] + ",";
+                if(temp < 0)
+                {
+                    break;
+                }
+                if(temp == 0)
+                {
+                    allPossibilities.Add(tempStr);
+                    break;
+                }
+                else
+                {
+
+                    dynamicAllPossiblitiesRecursive(tempStr, i, temp);
+                }
+            }
+        }
+
+        /*get the optimal solutions from all possibilities*/
+        public void findOptimalChange (int dynAmount, int[] denominations)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < optimal.GetLength(1); i++)
+            {
+                optimal[0, i] = i;
+                optCoin[0, i] = sb.ToString();
+                sb.Append(denominations[0] + " ");
+            }
+
+
+            for(int i = 1; i < denominations.Length; i++)
+            {
+                for(int j = 0; j < dynAmount + 1; j++)
+                {
+                    int value = j;
+                    int targetWithPrevDenomiation = optimal[i - 1, j];
+                    int ix = (value) - denominations[i];
+                    if(ix >= 0 && (denominations[i] <= value))
+                    {
+                        int x2 = denominations[i] + optimal[i, ix];
+                        if(x2 <= dynAmount && (1 + optimal[i, ix] < targetWithPrevDenomiation))
+                        {
+                            String temp = optCoin[i, ix] + denominations[i] + " ";
+                            optCoin[i, j] = temp;
+                            optimal[i, j] = 1 + optimal[i, ix];
+                        }
+                        else
+                        {
+                            optCoin[i, j] = optCoin[i - 1, j] + " ";
+                            optimal[i, j] = targetWithPrevDenomiation;
+                        }
+                    }
+                    else
+                    {
+                        optCoin[i, j] = optCoin[i - 1, j];
+                        optimal[i, j] = targetWithPrevDenomiation;
+                    }
+                }
+            }
+
+        }
+        /*method to call the recursive function*/
+        public void findAllPossibleCombinations (int dynAmount, int[] denominations)
+        {
+            string tempStr = "";
+            dynamicAllPossiblitiesRecursive(tempStr, 0, dynAmount);
+        }
+
+        /*initialization of data for Dynamic Solution*/
+        public void solutionCoins (int dynAmountx, int[] denominationsx)
+        {
+            dynAmount = dynAmountx;
+            denominations = denominationsx;
+            optCoin = new String[denominations.Length, dynAmount + 1];
+            optimal = new int[denominations.Length, dynAmount + 1];
+        }
+
+        #endregion
+
+
+        #region Greedy Methods
+
+        /*greedy method to get the minimum number of coins for given coinAmount*/
+        private void calcGreedy (int coinAmount)
+        {
+
+            
+            int[] coinArrayx = coins.ToArray();
+            Array.Reverse(coinArrayx);
+            int change = coinAmount;
+
+            foreach(int coinv in coinArrayx)
+            {
+                int noCoins = change / coinv;
+
+                if(noCoins == 0)
+                {
+                    continue;
+                }
+                change = change % coinv;
+                greedyCoins.Add(coinv, noCoins);
+
+                if(change == 0)
+                {
+                    break;
+                }
+            }
+            foreach(KeyValuePair<int, int> coinv in greedyCoins)
+            {
+                SetText3(coinv.Value + "  Coins of  denomination  " + coinv.Key + "\n");
+            }
+        }
+
+        #endregion
+
+
+
+
+        /*method to be used in dynamic functions thread */
+        private void dynmaicStart ()
+        {
+            solutionCoins(coinAmount, coins);
+            findAllPossibleCombinations(coinAmount, coins);
+            displayAllPossibilities();
+            findOptimalChange(coinAmount, coins);
+            SetText4(DynamicOptimal());
+        }
+
+        /*method to be used in greedy functions thread */
+        private void greedyStart ()
+        {
+            calcGreedy(coinAmount);
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+
+            try
+            {
+                List<int> tempc = new List<int>();
+                string[] temp = textBox5.Text.Split(' ');
+                for(int i = 0; i < temp.Length; i++)
+                {
+                    tempc.Add(Convert.ToInt32(temp[i]));
+                }
+                coins = tempc.ToArray();
+                Array.Sort(coins);
+
+            }
+            catch(Exception et)
+            {
+                MessageBox.Show("wrong input!");
+
+            }
+
+            if(run == false)
+            {
+                coinAmount = Convert.ToInt32(textBox1.Text);
+                Thread dynamic = new Thread(dynmaicStart);
+                dynamic.Start();
+                Thread greedy = new Thread(greedyStart);
+                greedy.Start();
+                run = true;
+            }
+            else
+            {
+                Form1 frm = new Form1();
+                this.Hide();
+                frm.Show();
+            }
+
+        }
+
+        #region GUI methods
+
+        delegate void SetTextCallback (string text);
+
+
+        private void SetText3 (string text)
+        {
+
+            if(this.textBox3.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText3);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox3.AppendText(text);
+
+            }
+        }
+
+        private void SetText2 (string text)
+        {
+
+            if(this.textBox2.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText2);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox2.AppendText(text);
+
+            }
+        }
+
+        private void SetText4 (string text)
+        {
+
+            if(this.textBox4.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText4);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox4.AppendText(text);
+
+            }
+        }
+
+    }
+
+        #endregion
+
+
+}
